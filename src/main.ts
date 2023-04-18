636354da1f665ec13b790283ccd84e206d2d93b0
baeniel <jinveloper126@gmail.com>
2023-04-18 23:06:17 +0900

    환경변수 load fix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
+import { config } from 'dotenv';
+// .env 파일 로드
+config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  // .env 파일 로드
-  dotenv.config();
-
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
