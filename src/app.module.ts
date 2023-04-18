1667bc302d7c789f280c3734a9ee5486d7ab9fd9
baeniel <jinveloper126@gmail.com>
2023-04-18 22:45:57 +0900

    supabase setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: 'SupabaseClient', useValue: supabase }],
 })
 export class AppModule {}
