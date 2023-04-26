0612ea0a04e3b9a5b38c469ac4a72d43442b3943
baeniel <jinveloper126@gmail.com>
2023-04-26 16:49:08 +0900

    class-validator에 걸리는 error 함께 처리

--- src/common/exceptions/http-exception.filter.ts
+++ src/common/exceptions/http-exception.filter.ts
@@ -13,29 +13,34 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    const error_code = exception.getResponse();
+    const errorResponse = exception.getResponse() as string | JSON;
     let message;
     let status;
 
-    switch (error_code) {
+    switch (errorResponse) {
       case 'PGRST116':
-        message = '결과가 존재하지 않습니다.';
+        message = '결과가 존재하지 않습니다';
         status = HttpStatus.NOT_FOUND;
         break;
       case '23505':
-        message = '이메일 중복.';
+        message = '이메일 중복';
         status = HttpStatus.CONFLICT;
         break;
       default:
-        message = '에러 발생';
         status = exception.getStatus();
     }
 
-    response.status(status).json({
-      statusCode: status,
-      message: message,
-      path: request.url,
-      timestamp: new Date().toISOString(),
-    });
+    if (typeof errorResponse == 'string') {
+      response.status(status).json({
+        statusCode: status,
+        message: message,
+        path: request.url,
+        timestamp: new Date().toISOString(),
+      });
+    } else {
+      response.status(status).json({
+        errorResponse,
+      });
+    }
   }
 }
